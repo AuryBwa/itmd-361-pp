74ea91d5cc46e9e7a66c5f700a5b35578465d396
Aury Bwashi <abwashi@hawk.iit.edu>
2016-11-14 21:39:36 -0600

    I finished pp-07 part 2

--- pp-07/site.js
+++ pp-07/site.js
@@ -1,22 +1,22 @@
 /*
-  1. Correct the variable scope so that the console.log call
-  outputs the correct value for x (5).
+1. Correct the variable scope so that the console.log call
+outputs the correct value for x (5).
 */
 
-var x;
-
-function double(num) {
-  x = num * 2;
-  x = 5;
-  return x;
-}
-
-double(6);
-console.log('The value of x is:', x, 'It should be 5.');
+(function(){
+  var x;
+  function double(num) {
+    x = num * 2;
+    x = 5;
+    return x;
+  }
+  double(6);
+  console.log('The value of x is:', x, 'It should be 5.');
+})();
 
 /*
-  2. Rewrite the corrected JavaScript above as function that
-  does not pollute the global namespace (e.g., neither its
-  variables nor the double function can be accessed via the
-  `window` global object, like `window.x`).
+2. Rewrite the corrected JavaScript above as function that
+does not pollute the global namespace (e.g., neither its
+variables nor the double function can be accessed via the
+`window` global object, like `window.x`).
 */
